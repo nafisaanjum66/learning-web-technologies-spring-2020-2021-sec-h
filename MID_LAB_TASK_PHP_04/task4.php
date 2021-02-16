868d1fa2b663c04111c801f7261def52d4285b9c
Dana <nafisaanjum2711@gmail.com>
2021-02-16 18:31:55 +0600

    4rth lab task php

---
+++ MID_LAB_TASK_PHP_04/task4.php
@@ -0,0 +1,14 @@
+<?php
+	$num1=14;
+	$num2=16;
+	$num3=18;
+	if($num1>$num2){
+		echo("number 1 is large");
+	}
+	elseif ($num2>$num3) {
+		echo "number 2 is large";
+	}
+	else{
+		echo "number 3 is large";
+	}
+?>
