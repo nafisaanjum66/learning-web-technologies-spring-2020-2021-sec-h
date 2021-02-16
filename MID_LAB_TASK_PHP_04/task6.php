0a1ede47ccb87082e57c2cb2fa04a9ba43c2b03e
Dana <nafisaanjum2711@gmail.com>
2021-02-16 19:11:42 +0600

    6th lab task php

---
+++ MID_LAB_TASK_PHP_04/task6.php
@@ -0,0 +1,19 @@
+<?php
+	$colors = array('key1' => 'Red', 'key2' => 'Green', 'key3' => 'Black');
+
+	$given_value = 'Black';
+
+	foreach ($colors as $value) {
+		echo "$value\t";
+	}
+	echo "\n";
+	if($value==$given_value)
+	{
+		echo "Element found";
+	}
+	else
+	{
+		echo "Element not found";
+	}
+
+?>
