767f20b09299e5a81303a1a9d68f7991af1ebf11
Dana <nafisaanjum2711@gmail.com>
2021-02-23 12:46:51 +0600

    task3 php6

---
+++ MID_LAB_TASK_PHP_06/TASK_3/file.php
@@ -0,0 +1,69 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Profile Picture</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<form method="POST" action="">
+	<fieldset>
+		<legend>PROFILE PICTURE</legend>
+
+		<table>
+			<tr>
+				<td><img src="icon.png" alt="icon" width="150px"></td>
+				
+			</tr>
+			<tr>
+				<td><input type="file" name = "file" value=""></td>
+			</tr>
+			
+		</table>
+
+		<hr>
+	<input type="submit" name="submit" value="Submit">	
+	</fieldset>	
+</form>
+	
+</body>
+</html>
+
+<?php
+
+	if (isset($_POST['submit'])) {
+		
+		
+		if(isset($_POST['file'])){
+
+			$filename = $_POST['file'];
+			echo "$filename <br>";
+
+			$ext = pathinfo($filename, PATHINFO_EXTENSION);
+			echo "$ext<br>";
+
+			//echo "Size : " . $_FILES['$filename']['size'] ."<br>";
+
+			$sizee = filesize("$filename");
+
+			echo "$sizee";
+
+
+			if($ext== "jpeg" || $ext== "jpg" || $ext== "png"){
+
+			echo "Done..!<br>";
+		}
+		else {
+		echo "Chose correct file.";
+	}
+			
+			
+		}else{
+
+			echo "Null Submission...<br>";
+		}
+		# code...
+	}
+?>
